6ca1b009da7cf0d6bf3091fea302db7abbff6958
v.mesheryakov <vm@custom-it.ru>
2019-03-25 10:25:51 +0400

    customize menu icons; add menu item in menu

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -7,44 +7,18 @@
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
                     ['label' => Yii::t('app', 'NAV_MENU'), 'options' => ['class' => 'header']],
-                    ['label' => Yii::t('app', 'NAV_USERS'), 'url' => '#', 'items' => [
-                        ['label' => Yii::t('app', 'NAV_USERS_LIST'), 'icon' => 'file-code-o', 'url' => ['/user']],
-                        ['label' => Yii::t('app', 'NAV_USERS_ROLES'), 'icon' => 'file-code-o', 'url' => ['/permit/access/role']],
-                        ['label' => Yii::t('app', 'NAV_USERS_ACCESS'), 'icon' => 'file-code-o', 'url' => ['/permit/access/permission']],
+                    ['label' => Yii::t('app', 'NAV_USERS'), 'icon' => 'user-o', 'url' => '#', 'items' => [
+                        ['label' => Yii::t('app', 'NAV_USERS_LIST'), 'url' => ['/user']],
+                        ['label' => Yii::t('app', 'NAV_USERS_ROLES'), 'url' => ['/permit/access/role']],
+                        ['label' => Yii::t('app', 'NAV_USERS_ACCESS'), 'url' => ['/permit/access/permission']],
                     ]],
-                    ['label' => Yii::t('app', 'NAV_MATERIAL'), 'url' => '#', 'items' => [
-                        ['label' => Yii::t('app', 'NAV_MATERIAL_CATEGORY'), 'icon' => 'file-code-o', 'url' => ['/category/default/index']],
-                        ['label' => Yii::t('app', 'NAV_MATERIAL_ARTICLE'), 'icon' => 'file-code-o', 'url' => ['/article/default/index']],
+                    ['label' => Yii::t('app', 'NAV_MATERIAL'), 'icon' => 'folder-open-o', 'url' => '#', 'items' => [
+                        ['label' => Yii::t('app', 'NAV_MATERIAL_CATEGORY'), 'url' => ['/category/default/index']],
+                        ['label' => Yii::t('app', 'NAV_MATERIAL_ARTICLE'), 'url' => ['/article/default/index']],
                     ]],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
+                    ['label' => Yii::t('app', 'NAV_MENU'), 'icon' => 'bars', 'url' => '#', 'items' => [
+
+                    ]],
                 ],
             ]
         ) ?>
